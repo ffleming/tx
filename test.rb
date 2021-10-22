ededda23a826efe32f093eaca97895f021218a8d
Forrest Fleming <ffleming@gmail.com>
2021-10-22 16:58:47 -0700

    remove useless test file

--- test.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-require 'httparty'
-
-str = File.read("state.json");
-j = JSON.parse(str)
-puts HTTParty.post("http://localhost:8080/radio", {headers: {'Content-Type' => 'application/json'}, body: j.to_json})
